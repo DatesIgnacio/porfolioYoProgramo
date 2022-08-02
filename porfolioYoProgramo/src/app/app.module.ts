cb43c118645c5808735b0434035b96bc58a7082d
DatesIgnacio <100616992+DatesIgnacio@users.noreply.github.com>
2022-08-01 21:33:57 -0300

    encabezado

--- porfolioYoProgramo/src/app/app.module.ts
+++ porfolioYoProgramo/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
+import { SocialesComponent } from './componentes/sociales/sociales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    LogoAPComponent,
+    SocialesComponent
   ],
   imports: [
     BrowserModule,
