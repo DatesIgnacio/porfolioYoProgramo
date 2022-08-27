043bfeb3e5f56f303319fd1fd6843ddd2e20a429
DatesIgnacio <100616992+DatesIgnacio@users.noreply.github.com>
2022-08-27 17:38:32 -0300

    avances al 27/8
    avances en el login

---
+++ porfolioYoProgramo/BackEnd I/ignacioDates/src/main/java/com/porfolioUno/ignacioDates/Security/Service/UserDetailsServiceImp.java
@@ -0,0 +1,22 @@
+package com.porfolioUno.ignacioDates.Security.Service;
+
+import com.porfolioUno.ignacioDates.Security.Entity.Usuario;
+import com.porfolioUno.ignacioDates.Security.Entity.UsuarioPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService{
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String nombreUsuario) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.getByNombreUsuario(nombreUsuario).get();
+        return UsuarioPrincipal.build(usuario);
+    }
+    
+}
